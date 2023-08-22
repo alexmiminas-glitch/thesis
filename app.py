f1482b8a53ff2a8d70c2d8f42299bf7f6053f7f1
Alexandros Miminas <140541033+alexmiminas-glitch@users.noreply.github.com>
2023-08-22 14:35:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,6 @@ if __name__ == '__main__':
     host = '0.0.0.0'
     
     # Use Gunicorn to run the app
-    cmd = f"gunicorn --bind={host}:{PORT} --threads=4 your_app_module_name:app"
+    cmd = f"gunicorn --bind={host}:{PORT} --threads=4 app:app"
     os.system(cmd)
 
