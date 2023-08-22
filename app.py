9dc910278e168ba27e5b206e8d844937eb9ead25
Alexandros Miminas <140541033+alexmiminas-glitch@users.noreply.github.com>
2023-08-22 14:09:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ def patient_info(patient_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
    
 
